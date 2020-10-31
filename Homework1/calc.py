babd5c8e7ff30e29dceac4b07c12f8ea4879ef1c
LiLi-scripts <72765576+LiLi-scripts@users.noreply.github.com>
2020-10-31 21:46:23 +0100

    Add files via upload

---
+++ Homework1/calc.py
@@ -0,0 +1,22 @@
+a=int(input("Digit 1: "))
+b=int(input("Digit 2: "))
+
+operator=input("Enter + or - or * or /: ")
+
+
+if operator =="+":
+    print(a+b)
+if operator =="-":
+    print(a-b)
+if operator =="*":
+    print(a*b)
+if operator =="/":
+    print(a/b)
+else:
+    print("end")
+    
+
+
+
+
+
